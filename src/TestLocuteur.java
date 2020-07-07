5a492554719807c39c691561e30f822365471dd2
Cheikh Tidiane Diedhiou <cheikht2011@gmail.com>
2020-07-07 13:24:25 +0200

    commentaires, renommage des variables et des noms de methodes de la classe TestLocuteur

--- src/TestLocuteur.java
+++ src/TestLocuteur.java
@@ -4,13 +4,18 @@ import java.io.FileReader;
 
 public class TestLocuteur {
 
+	/*
+		Methode permettant l'enregistrement audio pour le test avec Arecord.
+		Le locuteur à tester doit parler durant dix secondes en répétant le mot bonjour.
+	*/
 	private boolean enregistrement_wav_test() {
 		boolean retour = false;
-		// Trois minutes d'enregistrement
-		String command = "arecord --duration=5 ./alize/data/audios/test/test";
-		System.out.println("Bonjour, exprimez vous en repétant un mot. Vous avez 3 minutes");
+		// dix secondes d'enregistrement
+		String command = "arecord --duration=10 ./alize/data/audios/test/test";
+		System.out.println("Bonjour, exprimez vous en repétant le mot bonjour. Vous avez 10 secondes");
 		try
         { 
+        	// On arrete le systeme quelques minutes le temps de l'enregistrement
 			Thread.sleep(3000);
 			Runtime runtime = Runtime.getRuntime();
 			runtime.getRuntime().exec( command );
@@ -27,6 +32,10 @@ public class TestLocuteur {
 		return retour;
 	}
 	
+	/*
+		Methode realisant la parametrisation, la premiere normalisation, la detection d'energie
+		et la seconde normalisation.
+	*/
 	private boolean parametrisation_energyDetection_normalisation_test () {
 		
 		boolean retour = false;
@@ -39,14 +48,17 @@ public class TestLocuteur {
 			Runtime runtime = Runtime.getRuntime();
 			runtime.getRuntime().exec( command_parametrisation );
 			System.out.println("Parametrisation de test terminée");
+			// On arrete le systeme quelques minutes le temps de l'enregistrement
 			Thread.sleep(3000);
 			
 			runtime.getRuntime().exec( command_normalisation_1 );
 			System.out.println("Première normalisation de test terminée");
+			// On arrete le systeme quelques minutes le temps de l'enregistrement
 			Thread.sleep(3000);
 			
 			runtime.getRuntime().exec( command_energyDetector );
 			System.out.println("Détection d'énergie de test terminée");
+			// On arrete le systeme quelques minutes le temps de l'enregistrement
 			Thread.sleep(3000);
 			
 			runtime.getRuntime().exec( command_normalisation_2 );
@@ -64,6 +76,10 @@ public class TestLocuteur {
 		return retour;
 	}
 	
+	/*
+		Methode executant la commande compute_test. Commande qui réalise la comparaison le locuteur 
+		a identifier avec les autres modeles de locuteurs
+	*/
 	private boolean compute_test() {
 		boolean retour = false;
 		
@@ -73,6 +89,7 @@ public class TestLocuteur {
 			Runtime runtime = Runtime.getRuntime();
 			runtime.getRuntime().exec( command_compute );
 			System.out.println("Execution de  compute test terminée");
+			// On arrete le systeme quelques minutes le temps de l'enregistrement
 			Thread.sleep(3000);
 			retour = true;
         }
@@ -86,7 +103,9 @@ public class TestLocuteur {
 	}
 	
 	
-	
+		/*
+			Methode qui retourne le nombre de ligne présentes dans le fichier result.res
+		*/
 	   private int nombre_de_ligne()
 	   {
 	      // Le fichier d'entrée
@@ -116,13 +135,17 @@ public class TestLocuteur {
 	      return nbrLine; 
 	   }
 	   
+	   /* Methode qui parcourt le fichier result.res et ajoute renvoie le contenu sous forme de matreice
+		Chaque ligne du fichier est represente sous forme de ligne dans la matrice. et chaque mot
+		est stoque dans une case.
+	   */
 	   private String[][] lire_ligne ()
 	   {
 		   // Le fichier d'entrée
 		      File file = new File("./alize/res/result.res");
 		   // Initialiser le compteur à zéro
-		      int nbrLine = 0;
-		      int n = 0;
+		      int nbrLine = 0; // nombre de lignes dans result.res
+		      int indice = 0;
 		      nbrLine = nombre_de_ligne();
 		     
 		      String resultat[][] = new String[nbrLine][5];
@@ -133,11 +156,11 @@ public class TestLocuteur {
 				  // Créer l'objet BufferedReader 
 				  BufferedReader br = new BufferedReader(fr);  
 				  String str;
-				   // Lire le contenu du fichier
-				  while((str = br.readLine()) != null)
++				   // Lire le contenu du fichier
++				  while((str = br.readLine()) != null)
 				  {
-					 resultat[n] = str.split(" ");
-				     n++;               
+					 resultat[indice] = str.split(" ");
+				     indice++;               
 				  }
 				  fr.close();
 		      }
@@ -147,22 +170,35 @@ public class TestLocuteur {
 		      }
 		      return resultat;
 	   }
-	   
+	  
+	  	/*
+	  		Methode qui analyse le contenu du fichier result.res puis decide si 
+	  		la personne testée est identifiée ou non. La methode retourne le prenom et le nom de
+	  		la personne si elle est identifiée ou retourne null dans le cas contraire.
+	  	*/
 	   private String decision()
 	   {
+	   		// recuperation du contenu de result.res dans la matrice resultat
 		   String resultat[][] = lire_ligne();
-		   double valeurMax = 0.0;
+		   double valeurMax = 0.0; // Score max trouve
 		   int indiceDuMax = 0;
+		   // Pour chaque ligne du fichier result.res
 		   for (int i = 0; i < resultat.length; i++) {
+		   		// Si le troisieme element de la ligne est un 1
 				if(resultat[i][2].equals("1")) {
+					// Et si le 5eme element (la note) est superieure à la note maximale précédente
 					if (valeurMax < Double.parseDouble(resultat[i][4])) {
-						valeurMax = Double.parseDouble(resultat[i][4]);
-						indiceDuMax = i;
+						valeurMax = Double.parseDouble(resultat[i][4]); // On met à jour la note max
+						indiceDuMax = i; // On sauvegarde l'indice de la note max
 					}
 				}	
 			}
+
+			// Si la note max est superieur à 0.5 la personne est reconnu 
 		   if(Double.parseDouble(resultat[indiceDuMax][4]) > 0.5)
 		   {
+		   		// On extrait le nom et le prenom à partir de l'identifiant
+		   		// identifiznt = prenom_nom
 			   String prenomNom = resultat[indiceDuMax][1];
 			   prenomNom = prenomNom.replace("_", " ");
 			   return prenomNom; 
@@ -171,30 +207,29 @@ public class TestLocuteur {
 			   return null;
 		   
 	   }
-	
 
 	/*
+		Methode qui appele l'ensemble des methodes necessaires pour la réalisation d'un test
+	*/
 	boolean tester () {
+		
 		boolean retour = false;
+
+		String prenomNom = decision();
+		
 		if(enregistrement_wav_test ()) {
 			if(parametrisation_energyDetection_normalisation_test()) {
 				if(compute_test()) {
+					if(nom == null)
+						System.out.println("La personne qui a parlé n'est pas reconnu !");
+					else
+						System.out.println("La personne qui a parlé est : " + prenomNom);
 					retour = true;
 				}
 			}
 		}
 		return retour;
-	}*/
-	
-	boolean tester () {
-		String nom = decision();
-		
-		if(nom == null)
-			System.out.println("La personne qui a parlé n'est pas reconnu !");
-		else
-			System.out.println("La personne qui a parlé est : " + nom);
-		
-		return true;
 	}
+	
 	
 }
